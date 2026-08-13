ee93aae6e510418871fbb755922b482f8a2a7649
tasitom <mrleonthe1st@gmail.com>
2026-08-13 16:19:08 +0300

    fixed minor bug

--- src/main/java/main/Blocks/PedestalBlock.java
+++ src/main/java/main/Blocks/PedestalBlock.java
@@ -3,10 +3,6 @@ package main.Blocks;
 import main.Entities.PedestalEntity;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.ItemInteractionResult;
@@ -18,8 +14,6 @@ import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
-import net.neoforged.neoforge.common.NeoForgeConfig;
-import org.apache.logging.log4j.core.jmx.Server;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Set;
@@ -36,9 +30,6 @@ public class PedestalBlock extends Block implements EntityBlock {
 
     @Override
     protected ItemInteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitResult) {
-        ServerLevel hell = player.getServer().getLevel(ResourceKey.create(
-                Registries.DIMENSION,
-                ResourceLocation.fromNamespaceAndPath("hellbound", "hell")));
         if (!level.isClientSide) {
             BlockEntity be = level.getBlockEntity(pos);
             if (be instanceof PedestalEntity pedestal) {
