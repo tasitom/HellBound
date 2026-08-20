ee2b960482b8c886fe2c2362cbf20ec03f268814
KaraiskosDim <karaiskosd@icloud.com>
2026-08-20 20:50:02 +0300

    Added the proper particles to the CorruptedCandels

--- src/main/java/main/Particles/CandleParticles.java
+++ src/main/java/main/Particles/CandleParticles.java
@@ -15,8 +15,13 @@ public  class CandleParticles {
     public static final DeferredRegister<ParticleType<?>> PARTICLE_TYPES =
             DeferredRegister.create(BuiltInRegistries.PARTICLE_TYPE, Main.MODID);
 
-    public static final DeferredHolder<ParticleType<?>, SimpleParticleType> LUST_FLAME =
-            PARTICLE_TYPES.register("lust_flame", () -> new SimpleParticleType(false));
+    public static final DeferredHolder<ParticleType<?>, SimpleParticleType> ENVY_FLAME = PARTICLE_TYPES.register("envy_flame", () -> new SimpleParticleType(false));
+    public static final DeferredHolder<ParticleType<?>, SimpleParticleType> GLUTTONY_FLAME = PARTICLE_TYPES.register("gluttony_flame", () -> new SimpleParticleType(false));
+    public static final DeferredHolder<ParticleType<?>, SimpleParticleType> GREED_FLAME = PARTICLE_TYPES.register("greed_flame", () -> new SimpleParticleType(false));
+    public static final DeferredHolder<ParticleType<?>, SimpleParticleType> LUST_FLAME = PARTICLE_TYPES.register("lust_flame", () -> new SimpleParticleType(false));
+    public static final DeferredHolder<ParticleType<?>, SimpleParticleType> PRIDE_FLAME = PARTICLE_TYPES.register("pride_flame", () -> new SimpleParticleType(false));
+    public static final DeferredHolder<ParticleType<?>, SimpleParticleType> SLOTH_FLAME = PARTICLE_TYPES.register("sloth_flame", () -> new SimpleParticleType(false));
+    public static final DeferredHolder<ParticleType<?>, SimpleParticleType> WRATH_FLAME = PARTICLE_TYPES.register("wrath_flame", () -> new SimpleParticleType(false));
 
     public static void register(IEventBus eventBus){
         PARTICLE_TYPES.register(eventBus);
--- src/main/java/main/Particles/ParticleClientEvents.java
+++ src/main/java/main/Particles/ParticleClientEvents.java
@@ -12,7 +12,13 @@ public class ParticleClientEvents {
 
     @SubscribeEvent
     public static void registerParticleProviders(RegisterParticleProvidersEvent event) {
-        // Renders the custom lust_flame particle using flame behavior + our own sprite.
+        // Renders the custom candle flames using flame behavior + our own sprites.
+        event.registerSpriteSet(CandleParticles.ENVY_FLAME.get(), FlameParticle.Provider::new);
+        event.registerSpriteSet(CandleParticles.GLUTTONY_FLAME.get(), FlameParticle.Provider::new);
+        event.registerSpriteSet(CandleParticles.GREED_FLAME.get(), FlameParticle.Provider::new);
         event.registerSpriteSet(CandleParticles.LUST_FLAME.get(), FlameParticle.Provider::new);
+        event.registerSpriteSet(CandleParticles.PRIDE_FLAME.get(), FlameParticle.Provider::new);
+        event.registerSpriteSet(CandleParticles.SLOTH_FLAME.get(), FlameParticle.Provider::new);
+        event.registerSpriteSet(CandleParticles.WRATH_FLAME.get(), FlameParticle.Provider::new);
     }
 }
